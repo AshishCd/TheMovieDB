1a4ee4456044e63c45204619418259e1a0b30228
ashishcd <asurve18@yahoo.com>
2018-12-01 18:15:57 +0530

    whole files

---
+++ src/components/Hello.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+   <b-container fluid class="bv-example-row">
+    <b-row>
+        <b-col>1 of 3</b-col>
+        <b-col>2 of 3</b-col>
+        <b-col>3 of 3</b-col>
+    </b-row>
+</b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
